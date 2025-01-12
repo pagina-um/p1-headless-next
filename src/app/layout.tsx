00ef9ee28268dc2c65267e3928f7f0e8e7ce8123
Jose Pereira <jose.pereira@plumguide.com>
2025-01-12 23:20:39 +0000

    bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Header } from "@/components/layout/Header";
 import "@/styles/globals.css";
 import CookieConsent from "@/components/CookieConsent";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "News Grid",
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className="min-h-screen bg-gray-100">
+        <NextTopLoader color="#e10012" />
         <Header />
         {children}
         <CookieConsent />
